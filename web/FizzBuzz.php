503f5b3ae50a75f7ff26b0174ff8666af6c06059
Pongpop Inkaew <nice.comsci@gmail.com>
2018-09-03 19:57:56 +0700

    add green test

--- web/FizzBuzz.php
+++ web/FizzBuzz.php
@@ -6,13 +6,24 @@ class FizzBuzz {
 		if($this->divideByThree($numberTo)){
 			$sayWord = "Fizz";
 		}
-		if($numberTo == 5){
+		if($this->divideByFive($numberTo)){
 			$sayWord = "Buzz";
 		}
+		if($this->divideByThreeAndFive($numberTo)){
+			$sayWord = "FizzBuzz";
+		}
+
 		return $sayWord;
 	}
 	public function divideByThree ($numberTo){
 		return $numberTo % 3 == 0;
+	}
+	public function divideByFive ($numberTo){
+		return $numberTo % 5 == 0;
+	}
+
+	public function divideByThreeAndFive ($numberTo){
+		return $this->divideByThree($numberTo) && $this->divideByFive($numberTo); 
 	}
 }	
 ?>
