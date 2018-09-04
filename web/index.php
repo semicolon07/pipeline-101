8ade28dff41d9e9bb46ae5bd71b2f2a955323f43
Pongpop Inkaew <nice.comsci@gmail.com>
2018-09-04 09:22:42 +0700

    add link

--- web/index.php
+++ web/index.php
@@ -1,4 +1,4 @@
 <?php
 echo '<h1>CI/CD By Jenkins And Heroku</h1>';
-echo '<h2>Created By Uniga InfoTech</h2>';
+echo '<h2><a href="http://unigainfo.com" target="_blank"> Created By Uniga InfoTech</a></h2>';
 
